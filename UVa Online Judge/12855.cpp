58ffc4f2f76acec249a8042d48825eaecd0456a0
stroncod <stroncod@gmail.com>
2018-06-04 18:09:06 -0400

    change 12855 to get accepted in UVA

--- UVa Online Judge/12855.cpp
+++ UVa Online Judge/12855.cpp
@@ -30,17 +30,20 @@ WBWWBWBWBWBBBWWBBB
 W
 */
 int main(){
+
     long swp, refu;
-    long contador = 0;
     
     string palabra;
+    /*;
     cin >> swp;
     cin >> refu;
-    cin >> palabra;
+    cin >> palabra;*/
     
-    
-    
-    for(int i = 0; i < palabra.size();i++){
+    while(cin>>swp){
+      long contador = 0;
+          cin >> refu;
+          cin >> palabra;
+          for(int i = 0; i < palabra.size();i++){
         if(palabra[i] == 'B'){
             contador++;
         }
@@ -56,42 +59,46 @@ int main(){
     bool banBla = false;
         
     for(int i = (tamano-1); i >= 0; i--){
-    	if(palabra[i] == 'W'){
-	//			cout<<"i " << i << endl;
-    	  banNeg = true;    
+      if(palabra[i] == 'W'){
+  //      cout<<"i " << i << endl;
+        banNeg = true;    
       }
       if(palabra[j] == 'B'){
-	//			cout<<"j " << j << endl;
-    	  banBla = true;
+  //      cout<<"j " << j << endl;
+        banBla = true;
       }
       if(banBla && banNeg){
-      	//funcion swap
-				if(j-i == 1 || j-i == -1){
-							//cout<<"swapAy"<<endl;
-				  	  costo = costo + (swp-refu);
-					}else{
-				    if((j-i)*(swp-refu) < swp){
-							//cout<<"swapAy"<<endl;
-				  	  costo = costo + ((j-i)*(swp-refu));                    
-				    }else{
-							//cout<<"swap"<<endl;
-				  	  costo = costo + swp;
-				    }
-					}            
+        //funcion swap
+        if(j-i == 1 || j-i == -1){
+              //cout<<"swapAy"<<endl;
+              costo = costo + (swp-refu);
+          }else{
+            if((j-i)*(swp-refu) < swp){
+              //cout<<"swapAy"<<endl;
+              costo = costo + ((j-i)*(swp-refu));                    
+            }else{
+              //cout<<"swap"<<endl;
+              costo = costo + swp;
+            }
+          }            
         banNeg=false;
         banBla=false; 
       }else{
-      	if(banBla){
-        	j--;
+        if(banBla){
+          j--;
         }
         if(banNeg){
           i++;
         }
       }
       j++;
-	//		cout <<"costo "<<costo<<endl;
+  //    cout <<"costo "<<costo<<endl;
     }
-		cout << costo<< endl;
+    cout << costo<< endl;
+
+    }
+    
+    
        
     
     
